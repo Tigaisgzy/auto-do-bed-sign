e3d36b71ded56f32b7778faae9aaf7070a5dcf77
Tigaisgzy <119791838+Tigaisgzy@users.noreply.github.com>
2024-09-02 19:54:27 +0800

    更新校区判断逻辑

--- gzlg助手/goToBed.py
+++ gzlg助手/goToBed.py
@@ -3,7 +3,7 @@
 # @Time : 28/4/2024 下午8:33
 # @Author : G5116
 import re, execjs, json, requests, os, sys
-
+from datetime import datetime
 # 获取当前文件的目录
 current_dir = os.path.dirname(os.path.abspath(__file__))
 # 获取项目根目录
@@ -108,7 +108,7 @@ def doWork(session):
         'data': '{"SFFWN":"1","DDDM":"b2c1441606da4efbb9fe5b2b89226396","DDMC":"广州理工学院(博罗校区)","QDJD":114.08675193786623,"QDWD":23.186742693715477,"RWBH":"16FC8C91BCDDEC67E0630717000A97E1","QDPL":"2"}',
     }
     # logging.log(logging.INFO, '开始签到任务')
-    if int(os.getenv('USERNAME')[:4]) >= 2023:
+    if int(os.getenv('USERNAME')[:4]) >= datetime.now().year:
         print('定位hz')
         response = session.post(
             'https://xsfw.gzist.edu.cn/xsfw/sys/swmzncqapp/modules/studentCheckController/uniFormSignUp.do',
